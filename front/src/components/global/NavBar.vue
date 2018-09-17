52f8f2d421f8a28905bcbb71dda02e12cf150baf
lyle <lilyui@126.com>
2018-09-17 10:17:22 +0800

    add ip convert tool

--- front/src/components/global/NavBar.vue
+++ front/src/components/global/NavBar.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
     <el-menu
-      :default-active="activeIndex2"
       class="el-menu-demo"
       mode="horizontal"
-      @select="handleSelect"
       background-color="#545c64"
       text-color="#fff"
       active-text-color="#ffd04b">
-      <el-menu-item>
+      <el-menu-item index="1">
         <router-link to="/" class="banner" exact>
         Uiharu
         </router-link>
       </el-menu-item>
-      <el-menu-item>
+      <el-menu-item index="2">
         <router-link to="/">
           项目管理
         </router-link>
       </el-menu-item>
-      <el-menu-item>
+      <el-menu-item index="3">
         <router-link to="/tools">
           辅助工具
         </router-link>
--- front/src/layouts/tools.vue
+++ front/src/layouts/tools.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
     <nav-bar></nav-bar>
+    <!-- ip convert -->
+    <h3>IP 转换</h3>
+    <el-form label-width="100px">
+      <el-form-item label="普通形式">
+        <el-input v-model="rawip" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="八进制格式">
+        <el-input v-model="octip" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="十进制格式">
+        <el-input v-model="decip" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="十六进制格式">
+        <el-input v-model="hexip" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="十六进制整数">
+        <el-input v-model="hexintip" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+
     <foot-bar></foot-bar>
   </div>
 </template>
@@ -18,6 +38,51 @@ export default {
   },
   data () {
     return {
+      rawip: ''
+    }
+  },
+  computed: {
+    octip: function () {
+      var octip = this.rawip.split('.')
+      if (octip.length !== 4) {
+        return ''
+      }
+      return octip.map(
+        item => '0' + parseInt(item).toString(8)
+      ).join('.')
+    },
+    decip: function () {
+      var decip = this.rawip.split('.')
+      if (decip.length !== 4) {
+        return ''
+      }
+      var ret = 0
+      for (var i in decip) {
+        ret = ret << 8
+        ret += parseInt(decip[i])
+      }
+      return ret
+    },
+    hexip: function () {
+      var hexip = this.rawip.split('.')
+      if (hexip.length !== 4) {
+        return ''
+      }
+      return hexip.map(
+        item => '0x' + parseInt(item).toString(16)
+      ).join('.')
+    },
+    hexintip: function () {
+      var hexintip = this.rawip.split('.')
+      if (hexintip.length !== 4) {
+        return ''
+      }
+      var ret = 0
+      for (var i in hexintip) {
+        ret = ret << 8
+        ret += parseInt(hexintip[i])
+      }
+      return '0x' + ret.toString(16)
     }
   },
   methods: {
